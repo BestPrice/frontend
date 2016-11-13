e2574ea8709390b5e3e6804d1533c57121416f95
K4mc0 <bekec.kamil@gmail.com>
2016-11-13 16:30:46 +0100

    uprava indexu a upload idex2 a app2
    idex2 a app2 robila sasa

--- app/app2.js
+++ app/app2.js
@@ -3,7 +3,15 @@
  */
 var app=angular.module('App',[]);
 
-app.controller('statecontroler',['$scope','$http', function ($scope,$http) {
+app.controller('filterKat',['$scope','$http', function ($scope,$http) {
     $http.get("https://bestprice-backend.herokuapp.com/categories").success(function(response){$scope.states=response;});
+    $scope.update = function() {
+        console.log($scope.item.code, $scope.item.name)
+    }
+
 }]);
 
+app.controller('obchody',['$scope','$http', function ($scope,$http) {
+    $http.get("https://bestprice-backend.herokuapp.com/chainstores").success(function(response){$scope.states2=response;});
+
+}]);
