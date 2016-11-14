bc00a7c1adcd12cad5f0c7088b87a505c3b28a6e
K4mc0 <bekec.kamil@gmail.com>
2016-11-14 10:09:18 +0100

    sasa changes

--- app/app2.js
+++ app/app2.js
@@ -4,14 +4,33 @@
 var app=angular.module('App',[]);
 
 app.controller('filterKat',['$scope','$http', function ($scope,$http) {
-    $http.get("https://bestprice-backend.herokuapp.com/categories").success(function(response){$scope.states=response;});
-    $scope.update = function() {
-        console.log($scope.item.code, $scope.item.name)
-    }
+    $http.get("https://bestprice-backend.herokuapp.com/categories").success(function(response){
+            $scope.states=response;
+    });
+
+    $http.get("https://bestprice-backend.herokuapp.com/chainstores").success(function(response){
+        $scope.states2=response;
+    });
+
+    /*$scope.$watch('filter', function(newValue, oldValue){
+       console.log(newValue);
+    });
+
+    $scope.$watchGroup(['filter','obchody'], function(newValue, oldValue){
+        console.log(newValue);
+    });*/
+
+    $scope.obchody = [true, true, true,true];
+
+    $scope.$watchCollection('obchody', function(newValue, oldValue){
+        console.log(newValue);
+    });
+
+    $scope.$watch('keywords', function (newValue, oldValue){
+        $http.get("https://bestprice-backend.herokuapp.com/products?search="+ newValue).success(function(response){
+            $scope.data=response;
+        });
+    });
 
 }]);
 
-app.controller('obchody',['$scope','$http', function ($scope,$http) {
-    $http.get("https://bestprice-backend.herokuapp.com/chainstores").success(function(response){$scope.states2=response;});
-
-}]);
