cd68ce739cc7183ca1c2846cf57a8dcd33f605cc
K4mc0 <bekec.kamil@gmail.com>
2016-12-04 21:39:39 +0100

    dokoncenie vypoctu ceny + drobne fixy

--- app/app2.js
+++ app/app2.js
@@ -22,7 +22,8 @@ app.controller('filterKat',['$scope','$http', function ($scope,$http) {
     });
 
 	$scope.skuska={"stores":[{"chain_store_name":"Lidl","products":[{"id_product":"476ce886-ef33-4f06-a34b-1f01b5330292","product_name":"Biely Jogurt","brand_name":"Rajo","count":0,"price":"0"}]},{"chain_store_name":"Tesco","products":[{"id_product":"cedac768-da7e-4467-8876-a0b26adc5e4b","product_name":"Mňam dva duo - Vanilka","brand_name":"Rajo","count":1,"price":"0.32"},{"id_product":"01256ec5-8af0-4a7c-a367-8acc01c1184b","product_name":"Mňam dva duo - Čokoláda","brand_name":"Rajo","count":1,"price":"0.43"}]}],"shop_price_total":"0.75"}
-    $scope.obchody = [false, false, false,false];
+    $scope.obchody = [true, true, true,true];
+	$scope.vypocetCeny;
 
     $scope.$watch('obchody', function(newValue, oldValue){
         console.log(newValue);
@@ -88,16 +89,12 @@ app.controller('filterKat',['$scope','$http', function ($scope,$http) {
                 }
 
             } else {
+					product.count = 1;
                     $scope.cart.push(product); 
-                    if (!quantity) {
-                        $scope.cart.count.push(1);
-                        $scope.counter.push(1);
-
-                    } else{
-                        $scope.counter.push(quantity);
-
-                    }
-
+                    if (quantity == null) {
+						quantity = 1;
+                    } 
+                    $scope.counter.push(quantity);
                 }
          };
 
@@ -121,21 +118,19 @@ app.controller('filterKat',['$scope','$http', function ($scope,$http) {
 	
 	$scope.calculatePrice = function(cart){
 	
-		$scope.param = [];
-		 var x="{\"products\": [{\"id_product\": \"fa859c15-b89c-4b72-aace-8f09924cce39\",\"count\": 1},{\"id_product\": \"ef9929ce-40d4-47d4-b1a7-cde616df2a0c\",\"count\": 2},{\"id_product\": \"3b59192c-21a4-4e43-985c-33592b5fe4e4\",\"count\": 3}],\"user_preference\": {\"id_chain_stores\": [ \"6e38b271-321f-4df1-b8f7-87090b532f68\"],\"max_stores\": 1}}";
 		
-		/*for(var i = 0; i< $scope.cart.length; i++){
-			if($scope.cart[i].name == product.name){ = {}
-				repeat = true;
-				if (!quantity) {
-					$scope.counter[i]+=1;
-
-				} else{
-					$scope.counter[i]+=quantity;
-
-			}                        
-			}
-		}*/
+		$scope.param = [];
+		//test input
+		var x="{\"products\": [{\"id_product\": \"fa859c15-b89c-4b72-aace-8f09924cce39\",\"count\": 1},{\"id_product\": \"ef9929ce-40d4-47d4-b1a7-cde616df2a0c\",\"count\": 2},{\"id_product\": \"3b59192c-21a4-4e43-985c-33592b5fe4e4\",\"count\": 3}],\"user_preference\": {\"id_chain_stores\": [ \"6e38b271-321f-4df1-b8f7-87090b532f68\"],\"max_stores\": 1}}";
+		
+		var poleProduktov = [];
+		
+		for(var i = 0; i< $scope.cart.length; i++){
+			var jedenProdukt = {};
+			jedenProdukt["id_product"] = $scope.cart[i].id_product;
+			jedenProdukt["count"] = $scope.counter[i];
+			poleProduktov.push(jedenProdukt);
+		}
 		var poleObchodov = [];
 		if($scope.obchody[0]) poleObchodov.push("6e38b271-321f-4df1-b8f7-87090b532f68"); //Billa
 		if($scope.obchody[1]) poleObchodov.push("6259218a-cf1f-4d7e-b787-50db28de07f6"); //Lidl
@@ -145,17 +140,22 @@ app.controller('filterKat',['$scope','$http', function ($scope,$http) {
 		var parameter = {};
 		var user_preference = {};
 		user_preference["id_chain_stores"] = poleObchodov;
+		//defaultne nastavenie poctu obchodov na 1, ak si uzivatel nic nevybral. Nenasiel som na to funkciu tak to robim tu.
+		if($scope.pocetObchodov == null){
+			$scope.pocetObchodov=1;
+		}
 		user_preference["max_stores"] = $scope.pocetObchodov;
-		parameter["products"] = $scope.cart;
+		parameter["products"] = poleProduktov;
 		parameter["user_preference"] = user_preference;
 		
 		$http.post("https://bestprice-backend.herokuapp.com/shop",parameter).success(function(response){
-			$scope.states=response;
+			$scope.vypocetCeny=response;
 			//window.location.href='bestprice.html';
-			console.log("vystup je  = " + JSON.stringify(response));
+			console.log("vstup je  = " + JSON.stringify(parameter));
 			//console.log("produky su" + JSON.stringify($scope.cart));
-			console.log("vstup je " + JSON.stringify(parameter));
-			window.alert(response);
+			console.log("vystup je" + JSON.stringify($scope.vypocetCeny));
+			//console.log("produky sa " + JSON.stringify(poleProduktov));
+			//window.alert(response);
     });  
 	//window.alert("not success");
 		  
